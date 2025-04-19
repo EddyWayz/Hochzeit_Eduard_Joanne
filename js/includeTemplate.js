94f8d81bd4a1325304d33712a698993f576d83ce
eddywayz <eduard.wayz@gmail.com>
2025-04-19 22:54:01 +0200

    console path test

--- js/includeTemplate.js
+++ js/includeTemplate.js
@@ -2,4 +2,7 @@ $(function() {
     // Header und Footer in alle Seiten einfügen
     $('#header').load('../templates/header.html');
     $('#footer').load('../templates/footer.html');
+    console.log('Header und Footer geladen');
   });
+
+

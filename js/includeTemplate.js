52cf6b58b2a44c2f968b2c9970b79229f32d776f
eddywayz <eduard.wayz@gmail.com>
2025-04-21 23:29:38 +0200

    Enhances responsive design and adds RSVP styles
    Improves navigation for mobile devices with a collapsible menu.
    Adds new styles for the RSVP section for better user experience.
    Refactors header template to include a burger menu.
    Fixes missing newline and minor formatting issues.
    
    Improves usability on smaller screens and modernizes layout.

--- js/includeTemplate.js
+++ js/includeTemplate.js
@@ -2,6 +2,8 @@ $(function() {
     // Header und Footer in alle Seiten einfügen
     $('#header').load('html/templates/header.html');
     $('#footer').load('html/templates/footer.html');
-  });
+});
 
-
+$(document).on('click', '.menu-toggle', function() {
+  $('.nav-menu').toggleClass('open');
+});

eb30a5d7f85dd6f31088e5e3cb64a1b7a4c96a96
eddywayz <eduard.wayz@gmail.com>
2025-05-22 18:41:22 +0200

    Verbessert die Header-Template-Funktionalität: Fügt Event-Handler für das mobile Menü hinzu und optimiert das Schließen des Menüs bei Klicks außerhalb.

--- js/includeTemplate.js
+++ js/includeTemplate.js
@@ -1,10 +1,24 @@
 // includeTemplate.js: Lädt Header und Footer Templates und steuert das mobile Menü-Toggle
 $(function() {
-    // Header und Footer in alle Seiten einfügen
-    $('#header').load('html/templates/header.html');
-    $('#footer').load('html/templates/footer.html');
-});
+  // Header in alle Seiten einfügen und danach Event-Handler setzen
+  $('#header').load('html/templates/header.html', function() {
+    // Öffnen/Schließen per Klick auf das Burger-Icon
+    $(document).on('click', '.menu-toggle', function(e) {
+      e.stopPropagation(); // Verhindert, dass der folgende Document-Click-Handler sofort schließt
+      $('.nav-menu').toggleClass('open');
+    });
+    // Schließen, wenn außerhalb des Menüs geklickt wird
+    $(document).on('click', function(e) {
+      var $nav = $('.nav-menu');
+      // Nur schließen, wenn geöffnet und Klick weder auf das Menü noch auf das Toggle-Icon
+      if ($nav.hasClass('open') &&
+          !$(e.target).closest('.nav-menu').length &&
+          !$(e.target).closest('.menu-toggle').length) {
+        $nav.removeClass('open');
+      }
+    });
+  });
 
-$(document).on('click', '.menu-toggle', function() {
-    $('.nav-menu').toggleClass('open');
+  // Footer in alle Seiten einfügen
+  $('#footer').load('html/templates/footer.html');
 });

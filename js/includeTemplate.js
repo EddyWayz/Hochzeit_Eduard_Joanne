af27ebb23581a8cea040f9155afdd287cf6a4e17
eddywayz <eduard.wayz@gmail.com>
2025-05-22 18:50:01 +0200

    Aktualisiert die Versionsnummer in der Hero-Sektion von 1.19 auf 1.20 und verbessert die mobile Menü-Interaktion durch optimierte Event-Handler und CSS-Eigenschaften.

--- js/includeTemplate.js
+++ js/includeTemplate.js
@@ -1,24 +1,23 @@
-// includeTemplate.js: Lädt Header und Footer Templates und steuert das mobile Menü-Toggle
+// js/includeTemplate.js
+// Lädt Header und Footer und steuert das mobile Menü-Toggle
 $(function() {
-  // Header in alle Seiten einfügen und danach Event-Handler setzen
+  // Header laden und danach Event-Handler setzen
   $('#header').load('html/templates/header.html', function() {
-    // Öffnen/Schließen per Klick auf das Burger-Icon
-    $(document).on('click', '.menu-toggle', function(e) {
-      e.stopPropagation(); // Verhindert, dass der folgende Document-Click-Handler sofort schließt
+    // Öffnen/Schließen per Klick oder Touch auf das Burger-Icon
+    $(document).on('click touchstart', '.menu-toggle', function(e) {
+      e.stopPropagation(); // Verhindert sofortiges Schließen
       $('.nav-menu').toggleClass('open');
     });
-    // Schließen, wenn außerhalb des Menüs geklickt wird
-    $(document).on('click', function(e) {
+    // Schließen, wenn außerhalb des Menüs getippt/geclickt wird
+    $(document).on('click touchstart', function(e) {
       var $nav = $('.nav-menu');
-      // Nur schließen, wenn geöffnet und Klick weder auf das Menü noch auf das Toggle-Icon
       if ($nav.hasClass('open') &&
-          !$(e.target).closest('.nav-menu').length &&
-          !$(e.target).closest('.menu-toggle').length) {
+          !$(e.target).closest('.nav-menu, .menu-toggle').length) {
         $nav.removeClass('open');
       }
     });
   });
 
-  // Footer in alle Seiten einfügen
+  // Footer laden
   $('#footer').load('html/templates/footer.html');
 });
